e394470bf5c27b8bdc96ffc65648436f2e531795
Abel Gaming <lcpdpolice4939@gmail.com>
2023-03-30 02:05:28 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'cerulean'
 game 'gta5'
 description 'ESX Security Plus - Become a security guard of LS!'
 author 'Abel Gaming'
-version '1.0'
+version '1.2 DEVELOPMENT'
 
 server_scripts {
 	'config.lua',
